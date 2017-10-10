794c6619a43be6308504a53802e638cc6beb6fc8
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-10-11 00:40:20 +0200

    Computed correlation and pvalues

---
+++ correlation_analysis.R
@@ -0,0 +1,30 @@
+#Feature Selection/Engineering
+
+#Correlation analysis
+# http://www.sthda.com/english/wiki/correlation-matrix-a-quick-start-guide-to-analyze-format-and-visualize-a-correlation-matrix-using-r-software#compute-correlation-matrix
+
+
+install.packages("Hmisc");
+library(Hmisc)
+install.packages("stringi")
+library(stringi)
+
+source("C://Users//chris//OneDrive//Documentos//GitHub//photinus-analytics//loadAnswers.R");
+dataf =loadAnswers();
+summary(dataf)
+
+
+
+featuresdf<- data.frame(dataf$Answer.duration,dataf$Answer.confidence,dataf$Answer.difficulty,stri_length(dataf$Answer.explanation), 
+                        dataf$Worker.age,dataf$Worker.score,dataf$Worker.yearsOfExperience,
+                        dataf$Code.LOC,dataf$Code.complexity);
+featuresM<- data.matrix(featuresdf) #rcorr requires a matrix
+
+corOutput<- rcorr(featuresM,type  = "spearman");
+
+#Correlation coeficients
+corOutput$r
+
+
+#p-values
+corOutput$P

f31cbbbbd43250b9770e349d989a0d675d3ed42a
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-10-11 12:00:32 +0200

    Correlation and p-value tables

--- correlation_analysis.R
+++ correlation_analysis.R
@@ -18,13 +18,15 @@ summary(dataf)
 featuresdf<- data.frame(dataf$Answer.duration,dataf$Answer.confidence,dataf$Answer.difficulty,stri_length(dataf$Answer.explanation), 
                         dataf$Worker.age,dataf$Worker.score,dataf$Worker.yearsOfExperience,
                         dataf$Code.LOC,dataf$Code.complexity);
+colnames(featuresdf) <- c("Answer.duration","Answer.confidence","Answer.difficulty","Answer.explanationSize", 
+                          "Worker.age","Worker.score","Worker.yearsOfExperience",
+                          "Code.LOC","Code.complexity");
 featuresM<- data.matrix(featuresdf) #rcorr requires a matrix
 
 corOutput<- rcorr(featuresM,type  = "spearman");
 
 #Correlation coeficients
 corOutput$r
-
 
 #p-values
 corOutput$P
